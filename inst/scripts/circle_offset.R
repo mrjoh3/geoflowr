7d741f066486f45b61fc16bd1d72011992ed196e
Radoslaw Panczak <r.panczak@gmail.com>
2018-11-22 15:26:52 +1000

    fiddling with arrows

--- inst/scripts/circle_offset.R
+++ inst/scripts/circle_offset.R
@@ -4,12 +4,17 @@ library(tmap)
 tmap_mode("view")
 # tmap_mode("plot")
 
+set.seed(123)
+
 world <- read_csv("data-raw/world.csv") %>%
   select(admin, iso_n3, Longitude, Latitude)
 
 # world_sf <- st_as_sf(world, wkt = "the_geom")
 # qtm(world_sf)
 
+
+# using USA only for testing
+# numeric codes of countries do not work
 X201801 <- read_csv("data-raw/201801.csv") %>%
   filter(partner == "United States of America") %>%
   filter(partner != "World") %>%
@@ -33,18 +38,21 @@ dest_dot <- flows %>%
   group_by(reporter, dest_long, dest_lat) %>%
   summarise(netweight_total = sum(netweight_kg))
 
+# 'fake offset' using white fill & manual dest recalc
 ggplot()+
   geom_curve(data = flows,
-             aes(x = orig_long, y = orig_lat,
-                 xend = dest_long, yend = dest_lat),
+             aes(x = dest_long - (dest_long * runif(1,min=0.01,max=0.05)),
+                 y = dest_lat - (dest_lat * runif(1,min=0.01,max=0.05)),
+                 xend = orig_long,
+                 yend = orig_lat),
              arrow = arrow(angle = 3, ends = "first",type = "closed"),
              # size = log10(flows$netweight_kg),
              alpha = 0.5, curvature = 0.15) +
   geom_point(data = orig_dot,
-             aes(orig_long, orig_lat), size = 10,
-             shape=21) +
+             aes(orig_long, orig_lat), size = 5,
+             shape=21, fill = "white") +
   geom_point(data = dest_dot,
-             aes(dest_long, dest_lat), size = 10,
-             shape=21) +
+             aes(dest_long, dest_lat), size = 5,
+             shape=21, fill = "white") +
   theme_void()
 
