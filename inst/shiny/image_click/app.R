8be605154ddea5903f0eb9d2889f05c00c4e2126
Matthew Johnson <matthew.johnson@epa.vic.gov.au>
2018-11-27 10:06:49 +1100

    minimum example using mapedit to record coordinates

--- inst/shiny/image_click/app.R
+++ inst/shiny/image_click/app.R
@@ -1,22 +1,42 @@
 
+library(mapedit)
+library(mapview)
 library(shiny)
+library(raster)
+library(sf)
+library(affinething)
+
+# use this to set map extent
+world1 <- sf::st_as_sf(maps::map('world', plot = FALSE, fill = TRUE))
+map <- viewExtent(world1, alpha.regions = 0, stroke = FALSE)
 
 # Define UI for application that draws a histogram
 ui <- fluidPage(
 
    # Application title
    titlePanel("Click on an Image"),
-
-   shiny::imageOutput('image', clickId = 'imageClick', height = '400px'),
-   textOutput('text')
-
+   fluidRow(height = '600px',
+     column(6,
+            shiny::imageOutput('image', click = 'imageClick', height = '400px')),
+     column(6,
+            editModUI("editor", height = 600))
+   ),
+   fluidRow(height = '100px',
+            column(6,
+                   tableOutput('img_clicks')),
+            column(6,
+                   actionButton('btn', 'Georeference'))
+   ),
+   fluidRow()
 )
 
 
 server <- function(input, output) {
 
   # record clicked points
-  val <- reactiveValues(x=NULL, y=NULL)
+  img_pts <- reactiveValues(x=NULL, y=NULL)
+
+  geo_pts <- callModule(editMod, "editor", map@map)
 
   # Listen for clicks
   observe({
@@ -25,12 +45,33 @@ server <- function(input, output) {
       return()
     }
 
+    # third click will clear
     isolate({
-      val$x <- input$imageClick$x # c(val$x, input$rasterClick$x) use two clicks to creat XY points
-      val$y <- input$imageClick$y
+
+        img_pts$x <- c(img_pts$x, input$imageClick$x)
+        img_pts$y <- c(img_pts$y, input$imageClick$y)
+
     })
 
-    output$text <- renderText({paste0('X: ', val$x, ' -- ', 'Y: ', val$y)})
+    output$img_clicks <- renderTable({data.frame(img_pts$x,
+                                                 img_pts$y)})
+
+  })
+
+  # check inputs and georeference
+  observeEvent(input$btn, {
+
+    r <- brick('world.png')
+    xy <<- cbind(img_pts$x[1:2],
+                 nrow(r) - img_pts$y[1:2])
+
+    pts <<- st_coordinates(geo_pts()$finished)
+
+    withProgress(message = 'Georeferencing Image', {
+      rfix  <<- setExtent(r, affinething::domath(pts, xy, r = r))
+    })
+
+
   })
 
   output$image <- renderImage({
