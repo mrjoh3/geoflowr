2ffd4266a9ad71469a8f5c138a8d94aedef3f008
Matthew Johnson <matthew.johnson@epa.vic.gov.au>
2018-11-27 10:20:01 +1100

    incorporate affinething issue #1

--- inst/shiny/image_click/app.R
+++ inst/shiny/image_click/app.R
@@ -14,7 +14,15 @@ map <- viewExtent(world1, alpha.regions = 0, stroke = FALSE)
 ui <- fluidPage(
 
    # Application title
-   titlePanel("Click on an Image"),
+   titlePanel("Image Georeferencing"),
+   tags$p('Select points on the map and then click on the image. ',
+          'Pick a North East point first followed by a South West point. ',
+          'There is currently no error checking or validation. Then click ',
+          'the georeference button and wait. Close the app once complete; a raster called ',
+          'rfix will be in your environment. Plot this using: '),
+   tags$code('plotRGB(rfix);',
+             'maps::map(add = TRUE);',
+             'points(pts)'),
    fluidRow(height = '600px',
      column(6,
             shiny::imageOutput('image', click = 'imageClick', height = '400px')),
